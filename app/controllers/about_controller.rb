36156367c3878fd7a0b15a67e79f62ed32da2f91
triszt4n <trisztanpiller@gmail.com>
2021-01-31 22:40:22 +0100

    New colorways, overlay in progress

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,6 @@
+class AboutController < ApplicationController
+  # Index page for /about
+  def index
+    render :index
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_controller?
+
+  def current_controller?(names)
+    names.include?(params[:controller]) unless params[:controller].blank? || false
+  end
 end
