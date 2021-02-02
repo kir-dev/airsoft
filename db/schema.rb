fc798433de882f84b50589c1720d480227691302
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-02-02 08:33:49 +0100

    add omiauth
    add authsch
    
    add authsch

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,22 @@ ActiveRecord::Schema.define(version: 2021_02_01_184945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "admin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "provider"
+    t.string "uid"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   create_table "event_types", force: :cascade do |t|
     t.string "name"
     t.json "form_data"
@@ -40,12 +56,4 @@ ActiveRecord::Schema.define(version: 2021_02_01_184945) do
   end
 
   add_foreign_key "events", "posts"
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "authsch_id"
-    t.boolean "admin"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
