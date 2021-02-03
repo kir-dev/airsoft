6a372d1b22929474fb13f3afc5ae87669f85e0dc
poiuztrewq1 <bucsy.beni@gmail.com>
2021-02-03 18:37:47 +0100

    Add album design

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,10 @@
 class Album < ApplicationRecord
   belongs_to :post
   has_many_attached :images
+
+
+
+  def thumbnail
+    images.first.variant gravity: "Center", resize: "400x400^", crop: "400x400+0+0"
+  end
 end
