3fda4c200d9272c67637ebe25ca45782026fc141
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2021-02-19 04:13:21 -0800

    run album migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_073005) do
+ActiveRecord::Schema.define(version: 2021_02_10_233330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_073005) do
     t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_albums_on_post_id", unique: true
   end
 
   create_table "event_types", force: :cascade do |t|
