20235cb9a24f952ef9ac1d777b711046a602697d
Trisztán Piller <60111772+triszt4n@users.noreply.github.com>
2021-07-21 15:56:06 +0200

    Redesign the login and auth pages
    * Solve translation bug
    
    * Redesign devise pages and components
    
    * Add new menuitems to user dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def admin_signed_in?
     user_signed_in? and current_user.admin?
   end
+
+  def signed_in_via_omniauth?
+    user_signed_in? and current_user.provider == 'authsch'
+  end
 end
