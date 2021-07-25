e5abac7be097dae6ddfb359203c8ea018f4ac913
Beni <bucsy.beni@gmail.com>
2021-07-25 13:47:36 +0200

    Participations admin view

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,10 +1,25 @@
 class ParticipationsController < ApplicationController
   before_action :set_participation, only: %i[ show edit update destroy ]
   before_action :login_required
+  before_action :check_ownership, only: %i[show edit update destroy]
+  before_action :check_admin, only: %i[index]
 
-  # GET /participations
+  # GET /event/:event_id/registrations
+  # GET /event/:event_id/registrations.csv
   def index
-    @participations = Participation.all
+    @event          = Event.find(params[:event_id])
+    @participations = Participation.for_event params[:event_id]
+
+    if @event.event_type.nil?
+      redirect_to event_path(@event), notice: "Ehhez az eseményhez nincs jelentkező űrlap"
+    elsif @event.participations.empty?
+      redirect_to event_path(@event), notice: "Erre az eseményre még nem regisztráltak"
+    else
+      respond_to do |format|
+        format.html
+        format.csv { send_data helpers.generate_participation_csv(@event), filename: "participations_export-#{Date.today}.csv" }
+      end
+    end
   end
 
   # GET /participations/1
@@ -15,9 +30,9 @@ class ParticipationsController < ApplicationController
   def new
     event = Event.find(params[:event_id])
     if event.event_type.nil?
-      redirect_to root_url, notice: "Ehhez az eseményhez nincs jelentkező űrlap"
+      redirect_to event_path(event), notice: "Ehhez az eseményhez nincs jelentkező űrlap"
     elsif event.participations.exists?(user: current_user)
-      redirect_to root_url, notice: "Erre az eseményre már regisztráltál"
+      redirect_to event_path(event), notice: "Erre az eseményre már regisztráltál"
     else
       @event_type    = event.event_type
       @participation = Participation.new(event: event)
@@ -61,6 +76,13 @@ class ParticipationsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_participation
     @participation = Participation.find(params[:id])
+  end
+
+  def check_ownership
+    unless user_signed_in? and (current_user.admin? or current_user == @participation.user)
+      render(layout: "layouts/application", template: "layouts/_html_error",
+             locals: { code: 401, message: "Nincs hozzáférésed a tartalom kezeléséhez." })
+    end
   end
 
   # Only allow a list of trusted parameters through.
