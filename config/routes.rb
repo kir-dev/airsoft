8bd7b6663be7f8679a59653ee31ffc7d488d32d1
triszt4n <trisztanpiller@gmail.com>
2021-07-28 19:01:32 +0200

    Add archive, no more pagination needed

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,9 @@ Rails.application.routes.draw do
     member do
       delete :delete_image
     end
+    collection do
+      get :archive
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
