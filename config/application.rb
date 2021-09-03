62fc853b60641688d25704e6e65cc3fc7ca9cf33
poiuztrewq1 <bucsy.beni@gmail.com>
2021-09-03 19:12:02 +0200

    Solve timezone issue

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Airsoft
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.time_zone = 'Europe/Budapest'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
